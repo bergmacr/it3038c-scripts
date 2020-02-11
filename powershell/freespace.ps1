fbed07fcd51c7c8f4ee2390e89e221d3a4c0c40e
bergmacr <45783598+bergmacr@users.noreply.github.com>
2020-02-10 19:52:33 -0500

    Add files via upload
    project 1

---
+++ powershell/freespace.ps1
@@ -0,0 +1,8 @@
+﻿$driveinfo = Get-WMIobject win32_LogicalDisk -filter "DriveType=3" |
+                Select-Object SystemName, DeviceID, VolumeName,
+                @{Name="Size(GB)"; Expression={"{0:N1}" -f($_.size/1gb)}},
+                @{Name="FreeSpace(GB)"; Expression={"{0:N1}" -f($_.freespace/1gb)}},
+                @{Name="% FreeSpace(GB)"; Expression={"{0:N2}%" -f(($_.freespace/$_.size)*100)}}
+
+$driveinfo | Out-GridView 
+$driveinfo | Format-Table -AutoSize
