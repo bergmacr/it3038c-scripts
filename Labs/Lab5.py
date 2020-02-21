1ed137418f4964c12577779a6b1d249f5260efd6
bergmacr <45783598+bergmacr@users.noreply.github.com>
2020-02-21 14:30:57 -0500

    Update Lab5
    Indentation was wrong for first submission

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -1,15 +1,15 @@
-print("Enter a number and you will get all of the prime numbers of it!") 
-endNumber = int(input())                                         
+print("Enter a number to get the prime number for it") 
+endNumber = int(input()) 
 
-Beginning = 0                            
+Beginning = 0                          
 Conclusion = endNumber                   
 
 for prime in range(Beginning , Conclusion + 1):         
- if prime > 1:                                       
- for x in range(2, prime):
- if (prime % x) == 0:
- break
- else:
- print(prime)                  
+    if prime > 1:                                       
+        for x in range(2, prime):
+            if (prime % x) == 0:
+                break
+        else:
+            print(prime)                 
 
 print(endNumber)                       
