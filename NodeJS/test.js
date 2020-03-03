96744147cb3dce8f657186b1bc898621e2e55a3f
bergmacr <45783598+bergmacr@users.noreply.github.com>
2020-03-03 14:46:04 -0500

    Update test.js

--- NodeJS/test.js
+++ NodeJS/test.js
@@ -8,3 +8,4 @@ console.log("directory and file name: " + __filename);
 console.log("Using PATH module:");
 console.log('Hello from file ${path.basename(__filename)}');
 console.log('Process args: ${process.argv}')
+
