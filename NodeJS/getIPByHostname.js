298d9c60acc04de2e009fa04b5b04fad99d99a91
bergmacr <45783598+bergmacr@users.noreply.github.com>
2020-03-03 14:32:14 -0500

    Create getIPByHostname.js

---
+++ NodeJS/getIPByHostname.js
@@ -0,0 +1,16 @@
+var dns = require('dns');
+
+function hostnameLookup(hostname) {
+dns.lookup(hostname, function(err, addresses, family){
+console.log(addresses);
+});
+}
+
+if(process.argy.length <=2){
+	console.log("USAGE:"+ __filename + "IPADDR")
+	process.exit(-1)
+}
+
+var hostname = process.argy[2]
+console.log('Checking IP of:${hostname}')
+hostnameLookup(hostname);
