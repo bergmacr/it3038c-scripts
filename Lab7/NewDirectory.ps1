46743778f44621df61eca305b109c546f039ce58
bergmacr <45783598+bergmacr@users.noreply.github.com>
2020-03-10 15:02:34 -0400

    Create NewDirectory.ps1
    This file creates a new directory

---
+++ Lab7/NewDirectory.ps1
@@ -0,0 +1,3 @@
+#Here is how to create a directory. If a directory with the given name already exists, this command does nothing. It also creates any parent directories that may not have existed before.
+
+Install-Directory -Path 'C:\it3038c-scripts\Lab7\NewDirectory'
