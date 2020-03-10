48b930eeb73ffccd2d7ebf0282cbd62d12691859
bergmacr <45783598+bergmacr@users.noreply.github.com>
2020-03-10 15:04:08 -0400

    Create GetPermissions.ps1
    This file returns the permissions to the specified file, directory, etc.

---
+++ Lab7/GetPermissions.ps1
@@ -0,0 +1,3 @@
+#Here is how to get the permissions of a file, directory, etc. to a specified path. (To test this command yourself, replace the file path with whatever you would like, as I put in my own sample info.)
+
+Get-Permission -Path 'C:\it3038c-scripts\Lab7\permissions.txt'
